d33a96388ea06baac010d11940240ee7f8120926
komaji <cy11121@shibaura-it.ac.jp>
2015-06-21 02:15:44 +0900

    名前を揃える箇所を修正

--- sekigae.js
+++ sekigae.js
@@ -2,28 +2,14 @@
 
 var names = require("fs").readFileSync("./names.txt", "utf-8").trim().split("\n");
 for(var i = names.length - 1; i >= 0; i--){
-	var r = Math.floor(Math.random() * i);
-	var tmp = names[i];
-	names[i] = names[r];
-	names[r] = tmp;
+    var r = Math.floor(Math.random() * i);
+    var tmp = names[i];
+    names[i] = names[r];
+    names[r] = tmp;
 }
-var nlh =  Math.floor(names.length / 2);
-for(i = 0; i < nlh; i++){
-	var diff = names[i].length - names[i + nlh].length;
-	if(diff > 0){
-		var a = nlh;
-	}else if(diff < 0){
-		var a = 0;
-	}
-	names[i + a] += new Array(Math.abs(diff) + 1).join(" ");
-}
-var nstr = "";
-for(i = 0; i < names.length; i++){
-	nstr += names[i];
-	if(i == nlh - 1){
-		nstr += "\n";
-	}else if(i != names.length - 1){
-		nstr += " ";
-	}
-}
-console.log(nstr);
+var mlen = Math.max.apply(null, names.map(function(m){ return m.length; }));
+var hlen = Math.floor(names.length / 2);
+var out = names.map(function(name, i){
+    return (i == hlen - 1) ? name + '\n' : name + new Array((mlen - name.length) + 2).join(" ");
+}).join('');
+console.log(out);
