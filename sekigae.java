345267719e0c0304f64406927385eb3e91ef55b5
Kentaro Kuribayashi <kentarok@gmail.com>
2015-06-21 10:32:58 +0900

    Use `java.nio.*` instead

--- sekigae.java
+++ sekigae.java
@@ -1,32 +1,20 @@
 // javac sekigae.java && java Sekigae
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Sekigae {
-    public static void main (String[] args) {
-        List<String> names = loadFile("names.txt");
+class Sekigae {
+    public static void main (String[] args) throws Exception {
+        final List<String> names = Files.readAllLines(Paths.get("names.txt"), StandardCharsets.UTF_8);
         Collections.shuffle(names);
         eachSlice(names, 3).forEach((List<String> list) -> {
             System.out.println(list.stream().collect(Collectors.joining(" ")));
         });
-    }
-
-    private static List<String> loadFile(String path) {
-        List<String> list = new ArrayList<String>();
-
-        try {
-            FileReader fr = new FileReader(path);
-            BufferedReader br = new BufferedReader(fr);
-            list = br.lines().collect(Collectors.toList());
-        } catch(Exception e) {}
-
-        return list;
     }
 
     private static <T> List<List<T>> eachSlice(List<T> list, int size) {
