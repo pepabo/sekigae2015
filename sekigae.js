7585e31fb82ca43704e9a8f5d0b09f63928819b9
komaji <cy11121@shibaura-it.ac.jp>
2015-06-21 23:57:50 +0900

    入力ミス修正

--- sekigae.js
+++ sekigae.js
@@ -2,7 +2,7 @@
 
 var names = require("fs").readFileSync("./names.txt", "utf-8").trim().split("\n");
 for(var i = names.length - 1; i > 0; i--){
-    var r = Math.floor(Math.random() * (i + 1);
+    var r = Math.floor(Math.random() * (i + 1));
     var tmp = names[i];
     names[i] = names[r];
     names[r] = tmp;
