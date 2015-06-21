8fecb9525009199472aaac964777f42ff3800f98
komaji <cy11121@shibaura-it.ac.jp>
2015-06-21 23:55:56 +0900

    namesをシャッフルしている箇所の修正

--- sekigae.js
+++ sekigae.js
@@ -1,8 +1,8 @@
 // Usage: node sekigae.js
 
 var names = require("fs").readFileSync("./names.txt", "utf-8").trim().split("\n");
-for(var i = names.length - 1; i >= 0; i--){
-    var r = Math.floor(Math.random() * i);
+for(var i = names.length - 1; i > 0; i--){
+    var r = Math.floor(Math.random() * (i + 1);
     var tmp = names[i];
     names[i] = names[r];
     names[r] = tmp;
